d5c2feab0456dac751ccd85d441a572c65d5c927
EL-S <31227083+EL-S@users.noreply.github.com>
2018-11-28 23:58:39 +1100

    A working proxy retriever
    After x amount of loops through the list, the proxies refresh and update

--- proxies - working example.py
+++ proxies - working example.py
@@ -10,17 +10,56 @@ proxy_list = []
 wait = 2
 timeout_connect = 1
 timeout_read = 1
+proxy_loops = 0
 
-def request_url(url):
-    global wait
+def request_url(url,headers=None):
+    global wait,proxy_counter,use_proxies,proxy_list,proxy_loops
     try:
-        req = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"})
-        data = req.text
+        if proxy_loops >= 1:
+            if use_proxies == True:
+                print("Updating Proxies")
+                proxy_list = update_proxies()
+                print(proxy_list)
+            else:
+                proxy_list = None
+        if proxy_counter > (len(proxy_list)-1):
+            proxy_counter = 0
+            proxy_loops += 1
+    except:
+        proxy_counter = 0
+    try:
+        if use_proxies == True:
+            #proxy_ip = proxy_list[randint(0,len(proxy_list)-1)][0]
+            proxy_ip = proxy_list[proxy_counter][0]
+            print(proxy_counter,proxy_loops)
+            proxy = {"https":proxy_ip,"http":proxy_ip}
+            print("Proxy Ip:",proxy_ip)
+            proxy_counter += 1 #track position in the proxy list
+            if headers == None:
+                headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+            try:
+                headers["User-Agent"]
+            except:
+                headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+            req = requests.get(url, headers=headers, proxies=proxy,timeout=3)
+            data = req.text
+        else:
+            if headers == None:
+                headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+            try:
+                headers["User-Agent"]
+            except:
+                headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+            req = requests.get(url, headers=headers)
+            data = req.text
+        if data == None:
+            print("Page returned nothing! Reloading")
+            request_url(url,headers)
         return data
     except Exception as e:
-        print("Page failed to load! Reloading",e)
+        print("Page failed to load or Request timed out! Reloading",e)
         time.sleep(wait)
-        request_url(url)
+        request_url(url,headers)
 
 def update_proxies():
     unchecked_proxies = get_proxies()
@@ -32,7 +71,7 @@ def update_proxies():
     
 def get_proxies():
     unchecked_proxies = []
-    proxies_page = request_url("http://spys.one/en/anonymous-proxy-list/")
+    proxies_page = requests.get("http://spys.one/en/anonymous-proxy-list/").text
     soup = BeautifulSoup(proxies_page, "lxml")
     legend_code = soup.find("script", attrs={"type": "text/javascript"}).text.split(";")
     legend_code2 = []
@@ -93,18 +132,18 @@ def check_proxies(unchecked_proxies):
             time1 = datetime.datetime.now()
             req = requests.get(url, headers={"User-Agent": "fight me"}, proxies=proxy, timeout=(timeout_connect, timeout_read))
             time2 = datetime.datetime.now()
-            ping = int((time2 - time1).total_seconds() * 1000)
+            ping = str(int((time2 - time1).total_seconds() * 1000))+"ms"
             html = req.text
             soup = BeautifulSoup(html, "lxml")
             ip = soup.findAll("script", attrs={"type":"text/javascript"})[3].text.split("'\">")[1].split(r"</a>")[0]
             status = req.status_code
             if ip == host[0].split(":")[0]:
-                print("Working:",ping, status, ip, host[0])
+                print("Working:",ping, host[0])
                 checked_proxies.append(host)
             elif ip == current_ip:
                 print("Not Working:",host[0])
             else:
-                print("MultiLayered Working:",ping, status, ip, host[0])
+                print("MultiLayered Working:",ping, ip, host[0])
                 checked_proxies.append(host)
         except requests.exceptions. Timeout:
             print("Timed Out:",host[0])
@@ -115,6 +154,8 @@ def check_proxies(unchecked_proxies):
 if use_proxies == True:
     proxy_list = update_proxies()
     print(proxy_list)
+else:
+    proxy_list = None
 
 ##headers = {"Origin": "https://vidstreaming.io",
 ##           "Referer": "https://vidstreaming.io/",
@@ -122,18 +163,17 @@ if use_proxies == True:
 
 #referer vidstreaming.io
 
-headers = {"Origin": "http://vidstreaming.io",
-           "Referer": "http://vidstreaming.io/",
-           "User-Agent": "notlegitteehee 123/.0"}
+while True:
 
-proxy_ip = proxy_list[randint(0,len(proxy_list)-1)][0]
-print(len(proxy_list)-1)
-proxy = {"https":proxy_ip,"http":proxy_ip}
-print(proxy)
-#url = "http://hls12.mload.stream/hls/9a73328eaae6375b09e93541574a27b4/sub.4.m3u8"
-#url = "http://hls11x.mload.stream/hls/3cee59225bfe0e196edf87f828d8ce06/sub.12.360.m3u8"
-url = "http://hls11x.mload.stream/hls/3cee59225bfe0e196edf87f828d8ce06/sub.12.m3u8"
-res = requests.get(url, headers=headers, proxies=proxy)
-print(res)
-print(res.text)
+
+    headers = {"Origin": "http://vidstreaming.io",
+               "Referer": "http://vidstreaming.io/",
+               "User-Agent": "notlegitteehee 123/.0"}
+
+    #url = "http://hls12.mload.stream/hls/9a73328eaae6375b09e93541574a27b4/sub.4.m3u8"
+    #url = "http://hls11x.mload.stream/hls/3cee59225bfe0e196edf87f828d8ce06/sub.12.360.m3u8"
+    url = "http://hls11x.mload.stream/hls/3cee59225bfe0e196edf87f828d8ce06/sub.12.m3u8"
+    #res = requests.get(url, headers=headers, proxies=proxy)
+    data = request_url(url,headers=headers)
+    print(data)
 
